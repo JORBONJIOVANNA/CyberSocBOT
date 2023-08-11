2551607e076272df604683349976dbaee5a0ca79
Jordan <jmath0810@gmail.com>
2023-08-11 14:12:43 +1000

    Fixed the verification to only use 4-6 letter words

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os.path
 import sys
 from captcha.image import ImageCaptcha
 from random_word import RandomWords
+import re
+import random
 from classes import Captcha
 
 def read_token():
@@ -45,6 +47,9 @@ async def on_message(message):
                 return False
             mem_id = server.get_member(message.author.id)
             await mem_id.add_roles(role)
+            await mem_id.send("You've been successfully verified!!")
+            return True
+
         user = findUser(message.author)
         if not user is None:
             print(user.word)
@@ -58,15 +63,42 @@ async def on_message(message):
 
     await bot.process_commands(message)
 
-@bot.event
-async def on_member_join(member):
-    print(SERVER_ID)
-    r=RandomWords()
-
-    word = r.get_random_word()
+@bot.command()
+async def join_test(ctx):
+    def look_for_i_j_words(i,j):
+        with open("/usr/share/dict/words","r") as f:
+            f_contents = f.readlines()
+            words = re.findall(r'(\b\w{' +str(i) +r',' + str(j) +r'}\b)','\n'.join(f_contents))
+            return random.sample(words,1)[0]
+    member = ctx.author
 
     image = ImageCaptcha(width = 500, height = 100)
+    word = look_for_i_j_words(4,6)
+    image.generate_image(word)
+    count = 0
+    while os.path.exists(f"./tmp/{word}{'' if count ==0 else count}.png"):
+        count +=1
+    f_name = f"./tmp/{word}{count}.png"
+    image.write(word, f_name)
+    await member.send(file=nextcord.File(f_name))
+    newly_joined.append(Captcha(image,member,word))
 
+@bot.event
+async def on_member_join(member):
+    # r=RandomWords()
+
+    # word = r.get_random_word()
+
+    def look_for_number_words(num):
+        with open("/usr/share/dict/words","r") as f:
+            f_contents = f.readlines()
+            exp = "\b\w{1," + str(num) +"}\b"
+            words = re.findall(exp,f_contents)
+            return random.sample(words,1)[0]
+
+
+    image = ImageCaptcha(width = 500, height = 100)
+    word = look_for_number_words(5)
     image.generate_image(word)
     count = 0
     while os.path.exists(f"./tmp/{word}{'' if count ==0 else count}.png"):
